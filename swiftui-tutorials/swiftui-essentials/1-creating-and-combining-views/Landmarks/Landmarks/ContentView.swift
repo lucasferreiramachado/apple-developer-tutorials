8dd79f5346701e060df1efe066d6ac3f68f21b35
Lucas Ferreira Machado <lucasferreiramachadopro@gmail.com>
2023-08-07 19:46:08 -0300

    training(SwiftUI): [Section 1] Create a New Project and Explore the Canvas.

---
+++ swiftui-tutorials/swiftui-essentials/1-creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Lucas Ferreira Machado on 07/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, SwiftUI!")
+                .padding()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
