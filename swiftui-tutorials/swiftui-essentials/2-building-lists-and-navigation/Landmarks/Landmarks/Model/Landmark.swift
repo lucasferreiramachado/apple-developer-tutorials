f4d787b54a5a0d99ff0ed5e21dfebfdb21fad9d6
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-07 21:08:13 -0300

    training(SwiftUI): Section 1: Create a Landmark Model
    Section 1: Create a Landmark Model

---
+++ swiftui-tutorials/swiftui-essentials/2-building-lists-and-navigation/Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Lucas Ferreira Machado on 07/08/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    var image: Image {
+        Image(imageName)
+    }
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    private var imageName: String
+    private var coordinates: Coordinates
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
