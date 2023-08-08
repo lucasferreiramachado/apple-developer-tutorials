5e58bbceee6aa61dcb7340e191a7d27ed3214acd
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-07 22:16:06 -0300

    training(SwiftUI): Section 4 and Section 5
    Section 4: Use an Observable Object for Storage
    Section 5: Adopt the Model Object in Your Views

--- swiftui-tutorials/swiftui-essentials/3-handling-user-input/Landmarks/Landmarks/Views/LandmarkList.swift
+++ swiftui-tutorials/swiftui-essentials/3-handling-user-input/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
-    
+
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
     var body: some View {
         NavigationView {
             List {
-                
+
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                
+
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -34,7 +35,7 @@ struct LandmarkList: View {
             }
             .navigationTitle("Landmarks")
         }
-        
+
     }
 }
 
@@ -42,6 +43,7 @@ struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
+                .environmentObject(ModelData())
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
