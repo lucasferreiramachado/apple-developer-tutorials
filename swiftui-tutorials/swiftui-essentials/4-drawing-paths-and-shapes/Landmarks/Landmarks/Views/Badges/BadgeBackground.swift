d589f3304233787081b33d127341ca14814975bd
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-08 07:49:06 -0300

    training(SwiftUI): Drawing and Animation - Section 4
    Drawing and Animation - Section 4: Combine the Badge Foreground and Background

--- swiftui-tutorials/swiftui-essentials/4-drawing-paths-and-shapes/Landmarks/Landmarks/Views/Badges/BadgeBackground.swift
+++ swiftui-tutorials/swiftui-essentials/4-drawing-paths-and-shapes/Landmarks/Landmarks/Views/Badges/BadgeBackground.swift
@@ -14,32 +14,35 @@ struct BadgeBackground: View {
     var body: some View {
         GeometryReader { geometry in
             Path { path in
-                let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
                 path.move(
                     to: CGPoint(
-                        x: width * 0.95,
+                        x: width * 0.95 + xOffset,
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
-
-
+                
+                
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
-                            x: width * segment.line.x,
+                            x: width * segment.line.x + xOffset,
                             y: height * segment.line.y
                         )
                     )
-
-
+                    
+                    
                     path.addQuadCurve(
                         to: CGPoint(
-                            x: width * segment.curve.x,
+                            x: width * segment.curve.x + xOffset,
                             y: height * segment.curve.y
                         ),
                         control: CGPoint(
-                            x: width * segment.control.x,
+                            x: width * segment.control.x + xOffset,
                             y: height * segment.control.y
                         )
                     )
