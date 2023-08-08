f3c6d3e0f3bf74a058cf1aa73ca561c429378c50
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-07 21:14:53 -0300

    training(SwiftUI): Section 4: Create the List of Landmarks
    Section 4: Create the List of Landmarks

---
+++ swiftui-tutorials/swiftui-essentials/2-building-lists-and-navigation/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Lucas Ferreira Machado on 07/08/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
