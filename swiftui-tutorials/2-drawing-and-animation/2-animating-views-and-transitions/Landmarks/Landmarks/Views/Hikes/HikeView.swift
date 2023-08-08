8861583ceb2bf0ad28a483ec7400e8b4d7050ab9
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-08 08:45:19 -0300

    training(SwiftUI): Animating Views and Transitions - Section 3
    Animate the Effects of State Changes

--- swiftui-tutorials/2-drawing-and-animation/2-animating-views-and-transitions/Landmarks/Landmarks/Views/Hikes/HikeView.swift
+++ swiftui-tutorials/2-drawing-and-animation/2-animating-views-and-transitions/Landmarks/Landmarks/Views/Hikes/HikeView.swift
@@ -26,17 +26,16 @@ struct HikeView: View {
                 Spacer()
 
                 Button {
-                    showDetail.toggle()
+                    withAnimation(.easeInOut(duration: 4)) {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
-                        .animation(nil, value: showDetail)
                         .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
-                        .animation(.spring(), value: showDetail)
-
                 }
             }
 
