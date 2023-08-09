e74cc39ad18b37507e3ad3791cddb4d6c872fe2e
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-08 22:30:27 -0300

    training(SwiftUI): Composing Complex Interfaces - Section 1
    Add a Category View

---
+++ swiftui-tutorials/3-app-design-and-layout/1-composing-complex-interfaces/Landmarks/Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Lucas Ferreira Machado on 08/08/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
