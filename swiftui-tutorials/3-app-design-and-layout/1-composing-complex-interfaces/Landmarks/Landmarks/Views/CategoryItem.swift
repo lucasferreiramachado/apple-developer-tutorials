6d19a544b5427c055a76da056768b7bf8db674a2
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-08 22:40:13 -0300

    training(SwiftUI): Composing Complex Interfaces - Section 3
    Create a Category Row

---
+++ swiftui-tutorials/3-app-design-and-layout/1-composing-complex-interfaces/Landmarks/Landmarks/Views/CategoryItem.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Lucas Ferreira Machado on 08/08/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
