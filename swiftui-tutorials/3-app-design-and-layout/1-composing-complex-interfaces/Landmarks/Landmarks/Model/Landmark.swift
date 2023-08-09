392a4cf3fa8d23710222696030a99cbe0fd49db2
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-08 22:34:31 -0300

    training(SwiftUI): Composing Complex Interfaces - Section 2
    Create a Category List

--- swiftui-tutorials/3-app-design-and-layout/1-composing-complex-interfaces/Landmarks/Landmarks/Model/Landmark.swift
+++ swiftui-tutorials/3-app-design-and-layout/1-composing-complex-interfaces/Landmarks/Landmarks/Model/Landmark.swift
@@ -10,12 +10,14 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable {
+
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
     var isFavorite: Bool
+    var category: Category
 
     var image: Image {
         Image(imageName)
@@ -29,8 +31,22 @@ struct Landmark: Hashable, Codable, Identifiable {
 
     private var imageName: String
     private var coordinates: Coordinates
+}
+
+extension Landmark {
+
+    enum Category: String, CaseIterable, Codable {
+
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+}
+
+extension Landmark {
 
     struct Coordinates: Hashable, Codable {
+
         var latitude: Double
         var longitude: Double
     }
