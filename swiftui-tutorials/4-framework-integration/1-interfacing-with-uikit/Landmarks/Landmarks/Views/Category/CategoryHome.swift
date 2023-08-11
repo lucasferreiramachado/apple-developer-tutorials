aa2b25f369c93386db89da3309d61d3ef5fd650a
Lucas Ferreira Machado <81539770+lucasferreiramachado@users.noreply.github.com>
2023-08-10 22:41:09 -0300

    training(SwiftUI): Interfacing with UIKit - Section 4
    Add a custom UIPageControl wrapped in SwiftUI UIViewRepresentable view.

--- swiftui-tutorials/4-framework-integration/1-interfacing-with-uikit/Landmarks/Landmarks/Views/Category/CategoryHome.swift
+++ swiftui-tutorials/4-framework-integration/1-interfacing-with-uikit/Landmarks/Landmarks/Views/Category/CategoryHome.swift
@@ -8,20 +8,17 @@
 import SwiftUI
 
 struct CategoryHome: View {
-
+    
     @EnvironmentObject var modelData: ModelData
     @State private var showingProfile = false
-
+    
     var body: some View {
         NavigationView {
             List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    .aspectRatio(3 / 2, contentMode: .fit)
                     .listRowInsets(EdgeInsets())
-
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
@@ -45,7 +42,7 @@ struct CategoryHome: View {
 }
 
 struct CategoryHome_Previews: PreviewProvider {
-
+    
     static var previews: some View {
         CategoryHome()
             .environmentObject(ModelData())
